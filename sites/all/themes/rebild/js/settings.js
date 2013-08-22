7f458baa1b38fb0c1c17b3c4cdcbe117bb65ba61
Henrik Bak Thomassen <henrik@createinside.com>
2013-08-22 17:15:58 +0200

    Selfservice styling and template file

--- sites/all/themes/rebild/js/settings.js
+++ sites/all/themes/rebild/js/settings.js
@@ -7,6 +7,28 @@ $(function() {
 	
 	// Swipebox init
 	$(".swipebox").swipebox();
+	
+	// Selfservice accordion - Topic list level 1
+	$(".view-selfservice-items.view-display-id-emner").accordion({
+    header: ".view-grouping-header",
+    autoHeight: false,
+    collapsible: true,
+    active: false
+	});
+	// Selfservice accordion - Topic list level 2
+	$(".view-selfservice-items.view-display-id-emner .view-grouping-content").accordion({
+    header: "h3",
+    autoHeight: false,
+    collapsible: true,
+    active: false
+	});
+	// Selfservice accordion - Alphabetical list level 1
+	$(".view-selfservice-items.view-display-id-alfabetisk").accordion({
+    header: "h3",
+    autoHeight: false,
+    collapsible: true,
+    active: false
+	});
 
 });
 
