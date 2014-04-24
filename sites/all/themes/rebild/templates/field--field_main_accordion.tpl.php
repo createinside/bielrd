8996ef98e7cafbe9a9121780970e9f0776c4068f
Henrik Bak Thomassen <henrik@createinside.com>
2014-04-24 09:16:08 +0200

    Fixed php syntax error in accordion template file

--- sites/all/themes/rebild/templates/field--field_main_accordion.tpl.php
+++ sites/all/themes/rebild/templates/field--field_main_accordion.tpl.php
@@ -44,12 +44,6 @@
  * @ingroup themeable
  */
 ?>
-<!--
-THIS FILE IS NOT USED AND IS HERE AS A STARTING POINT FOR CUSTOMIZATION ONLY.
-See http://api.drupal.org/api/function/theme_field/7 for details.
-After copying this file to your theme's folder and customizing it, remove this
-HTML comment.
--->
 
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
@@ -57,7 +51,7 @@ HTML comment.
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-    	<? 
+    	<?php 
     	$hash_class = "";
     	
     	if(isset($item["entity"])) {
