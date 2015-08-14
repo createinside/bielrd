c3804303c4fc642cf335ddb9bd8643d2704eebe7
Henrik Bak Thomassen <henrik@createinside.com>
2015-08-14 14:23:04 +0200

    Moved JS to Drupal.behaviours in Media Browser UX module

--- sites/all/modules/createinside/modules/media_browser_ux/js/media_browser_ux.js
+++ sites/all/modules/createinside/modules/media_browser_ux/js/media_browser_ux.js
@@ -1,7 +1,23 @@
 $ = jQuery;
 
+/**
+ * Callback Media => Linkit
+ *
+ * Populates the linkit form with information from media file
+ */
+function mediaLinkit(file) {
+		
+	link = {
+		path: file[0].url,
+		attributes: {}
+	};
+	link.attributes['class'] = 'file-link-'+file[0].fid;
+		
+	Drupal.linkit.dialog.populateFields(link);
+}
+
 $(function() {
-	
+		
 	/**
 	 * Change Event
 	 *
@@ -26,88 +42,58 @@ $(function() {
 		$(".ui-draggable").removeClass('selected');
 		$(this).closest(".ui-draggable").addClass("selected");
 	});
-		
-	/**
-	 * Click Event
-	 *
-	 * Makes the links inside draggable elements clickable (Instead of selecting the whole row)
-	 */
-	 
-	// File action links
-	$('.doc-actions a, .doc-user a, .doc-use a').click(function(e) { 
-		e.preventDefault();
-		var href = $(this).attr('href');
-    window.location.href = href; 
-		return false;
-	});
-	$('.media-thumbnail a').click(function(e) {
-		e.preventDefault();
-		var href = $(this).attr('href');
-		window.open(
-		  href,
-		  '_blank'
-		);
-	});
-	
-	Drupal.behaviors.media_browser_ux = {
-    attach: function (context) {
-			$('.doc-actions a, .doc-user a, .doc-use a').click(function(e) { 
-				e.preventDefault();
-				var href = $(this).attr('href');
-		    window.location.href = href; 
-				return false;
-			});	
-			$('.media-thumbnail a').click(function(e) {
-				e.preventDefault();
-				var href = $(this).attr('href');
-				window.open(
-				  href,
-				  '_blank'
-				);
-			});
-			// Focus searchfield in views exposed filters
-			if($("#edit-filename").length!=0) {
-				if($("#edit-filename").val()) {
-					$("#edit-filename").focus();
-					var tmpStr = $("#edit-filename").val();
-					$("#edit-filename").val('');
-					$("#edit-filename").val(tmpStr);
-				}
-			}
-	  }
-	}	
-
-	/**
-	 * Open the Media file browser inside LinkIt
-	 */
-	if(typeof Drupal.linkit != "undefined") {
-		Drupal.linkit.dialog.openFileBrowser = function () {
-			
-			// Settings for the modal window
-			settings = new Object();
-			settings.enabledPlugins = ["media_browser_plus_customized--media_browser_thumbnails","upload"];
-			settings.file_directory = "wysiwyg_media_files";
-			settings.id = "media_linkit_wysiwyg";
-			
-			// Opens the modal window
-			Drupal.media.popups.mediaBrowser(mediaLinkit, settings);
-		};
-	}
-	
 });
 
-/**
- * Callback Media => Linkit
- *
- * Populates the linkit form with information from media file
- */
-function mediaLinkit(file) {
+Drupal.behaviors.media_browser_ux = {
+  attach: function (context) {
+    
+		/**
+		 * Open the Media file browser inside LinkIt
+		 */
+    if(typeof Drupal.linkit != "undefined") {
+			Drupal.linkit.dialog.openFileBrowser = function () {
+				
+				// Settings for the modal window
+				settings = new Object();
+				settings.enabledPlugins = ["media_browser_plus_customized--media_browser_thumbnails","upload"];
+				settings.file_directory = "wysiwyg_media_files";
+				settings.id = "media_linkit_wysiwyg";
+				
+				// Opens the modal window
+				Drupal.media.popups.mediaBrowser(mediaLinkit, settings);
+			};
+		}
+    
+		/**
+		 * Click Event
+		 *
+		 * Makes the links inside draggable elements clickable (Instead of selecting the whole row)
+		 */
+		 
+		// File action links
+		$('.doc-actions a, .doc-user a, .doc-use a').click(function(e) { 
+			e.preventDefault();
+			var href = $(this).attr('href');
+	    window.location.href = href; 
+			return false;
+		});
+		$('.media-thumbnail a').click(function(e) {
+			e.preventDefault();
+			var href = $(this).attr('href');
+			window.open(
+			  href,
+			  '_blank'
+			);
+		});
 		
-	link = {
-		path: file[0].url,
-		attributes: {}
-	};
-	link.attributes['class'] = 'file-link-'+file[0].fid;
-		
-	Drupal.linkit.dialog.populateFields(link);
+		// Focus searchfield in views exposed filters
+		if($("#edit-filename").length!=0) {
+			if($("#edit-filename").val()) {
+				$("#edit-filename").focus();
+				var tmpStr = $("#edit-filename").val();
+				$("#edit-filename").val('');
+				$("#edit-filename").val(tmpStr);
+			}
+		}  
+  }
 }
