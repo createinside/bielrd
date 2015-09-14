49bcc09b4de332da4149ca1c1af3d124fa46caf6
christianhg <christianhg@gmail.com>
2015-09-14 11:36:52 +0200

    Edit main email template

--- sites/all/modules/createinside/modules/nl_br/templates/nl_br_export_markup.tpl.php
+++ sites/all/modules/createinside/modules/nl_br/templates/nl_br_export_markup.tpl.php
@@ -123,101 +123,105 @@
 	<table bgcolor="#b7b7b7" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
 		<tbody>
 			<tr>
-				<td>&nbsp;</td>
-				<td width="600">
-					<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
-						<tbody>
-							<tr>
-								<td width="600" height="15">&nbsp;</td>
-							</tr>
-						</tbody>
-					</table>
-					<!-- <HEADER> -->
-					<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
-						<tbody>
-							<tr>
-								<td width="600" height="25" style="font-family: Arial, sans-serif; font-size: 11px; color: #282828; text-align: center;">Vises denne mail ikke korrekt? <a href="%%webversion%%" style="text-decoration: none; color: #282828">Åbn i din browser - Klik her</a></td>
-							</tr>
-						</tbody>
-					</table>
-					<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
-						<tbody>
-							<tr>
-								<td width="600" height="15">&nbsp;</td>
-							</tr>
-						</tbody>
-					</table>
-					<table bgcolor="#282828" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
-						<tbody>
-							<tr>
-								<td width="30" height="15">&nbsp;</td>
-								<td width="270" height="50" valign="middle" align="right" style="font-family: Arial, sans-serif; font-size: 20px; text-align: left;">
-									<a style="text-decoration: none;" target="_blank" href="http://rebild.dk/erhverv"><span style="color:#d9620c ;">BUSINESS </span><span style="color:#ffffff;">REBILD</span></a>
-								</td>
-								<td width="270" valign="middle" align="right" style="font-family: Arial, sans-serif; font-size: 12px; line-height: 14px; color: #ffffff; text-align: right;">
-									<a href="http://rebild.dk/nyheder" style="color: #ffffff;text-decoration: none;">Aktuelt</a> &nbsp;|&nbsp;
-									<a href="http://rebild.dk/kontakt" style="color: #ffffff;text-decoration: none;">Kontakt</a> &nbsp;|&nbsp;
-									<a href="https://www.linkedin.com/company/business-rebild" style="color: #ffffff;text-decoration: none;">LinkedIn</a>
-								</td>
-								<td width="30" height="15">&nbsp;</td>
-							</tr>
-						</tbody>
-					</table>
-					<!-- </HEADER> -->
-					<!-- <CONTENT> -->
-					<?php
-						foreach($variables['sections'] as $section) {
-							print $section;
-						}
-					?>
-					<!-- </CONTENT> -->
-					<!-- <FOOTER> -->
-					<table bgcolor="#d9620c" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
-						<tbody>
-							<tr>
-								<td>&nbsp;</td>
-								<td width="72" height="25">&nbsp;</td>
-								<td>&nbsp;</td>
-							</tr>
-							<tr>
-								<td>&nbsp;</td>
-								<td align="center" width="72" style="font-family: Arial, sans-serif; font-size: 18px; color: #282828; text-align: center;"><img width="72" border="0" alt="" style="display:block; border:none; outline:none; text-decoration:none;" src="<?php print $base_url."/".drupal_get_path("module", "nl_br"); ?>/img/logo.png"></td>
-								<td>&nbsp;</td>
-							</tr>
-							<tr>
-								<td>&nbsp;</td>
-								<td width="72" height="25">&nbsp;</td>
-								<td>&nbsp;</td>
-							</tr>
-						</tbody>
-					</table>
-					<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
-						<tbody>
+				<td align="center" valign="top">
+					<table bgcolor="#b7b7b7" border="0" cellpadding="0" cellspacing="0" width="600">
 						<tr>
-							<td width="600" height="25">&nbsp;</td>
-						</tr>
-						<tr>
-							<td width="600" style="font-family: Arial, sans-serif; font-size: 11px; line-height: 14px; color: #282828; text-align: center;">
-								<p>
-									&Oslash;nsker du at afmelde %%emailaddress%% fra denne liste - <a shref="%%unsubscribelink%%" style="text-decoration: underline; color: #282828">klik her</a>.
+							<td align="center" valign="top">
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="600" height="15">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+								<!-- <HEADER> -->
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="600" height="25" style="font-family: Arial, sans-serif; font-size: 11px; color: #282828; text-align: center;">Vises denne mail ikke korrekt? <a href="%%webversion%%" style="text-decoration: none; color: #282828">Åbn i din browser - Klik her</a></td>
+										</tr>
+									</tbody>
+								</table>
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="600" height="15">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+								<table bgcolor="#282828" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="30" height="15">&nbsp;</td>
+											<td width="270" height="50" valign="middle" align="right" style="font-family: Arial, sans-serif; font-size: 20px; text-align: left;">
+												<a style="text-decoration: none;" target="_blank" href="http://rebild.dk/erhverv"><span style="color:#d9620c ;">BUSINESS </span><span style="color:#ffffff;">REBILD</span></a>
+											</td>
+											<td width="270" valign="middle" align="right" style="font-family: Arial, sans-serif; font-size: 12px; line-height: 14px; color: #ffffff; text-align: right;">
+												<a href="http://rebild.dk/nyheder" style="color: #ffffff;text-decoration: none;">Aktuelt</a> &nbsp;|&nbsp;
+												<a href="http://rebild.dk/kontakt" style="color: #ffffff;text-decoration: none;">Kontakt</a> &nbsp;|&nbsp;
+												<a href="https://www.linkedin.com/company/business-rebild" style="color: #ffffff;text-decoration: none;">LinkedIn</a>
+											</td>
+											<td width="30" height="15">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+								<!-- </HEADER> -->
+								<!-- <CONTENT> -->
+								<?php
+									foreach($variables['sections'] as $section) {
+										print $section;
+									}
+								?>
+								<!-- </CONTENT> -->
+								<!-- <FOOTER> -->
+								<table bgcolor="#d9620c" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td>&nbsp;</td>
+											<td width="72" height="25">&nbsp;</td>
+											<td>&nbsp;</td>
+										</tr>
+										<tr>
+											<td width="264" height="15">&nbsp;</td>
+											<td align="center" width="72" style="font-family: Arial, sans-serif; font-size: 18px; color: #282828; text-align: center;"><img width="72" border="0" alt="" style="display:block; border:none; outline:none; text-decoration:none;" src="<?php print $base_url."/".drupal_get_path("module", "nl_br"); ?>/img/logo.png"></td>
+											<td width="264" height="15">&nbsp;</td>
+										</tr>
+										<tr>
+											<td>&nbsp;</td>
+											<td width="72" height="25">&nbsp;</td>
+											<td>&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+									<tr>
+										<td width="600" height="25">&nbsp;</td>
+									</tr>
+									<tr>
+										<td width="600" style="font-family: Arial, sans-serif; font-size: 11px; line-height: 14px; color: #282828; text-align: center;">
+											<p>
+												&Oslash;nsker du at afmelde %%emailaddress%% fra denne liste - <a shref="%%unsubscribelink%%" style="text-decoration: underline; color: #282828">klik her</a>.
 
-									<br>
-									<br>Copyright &copy; <?php echo date('Y') ?> Business Rebild
-									<br>Østre Alle 6, 9530 St&oslash;vring
+												<br>
+												<br>Copyright &copy; <?php echo date('Y') ?> Business Rebild
+												<br>Østre Alle 6, 9530 St&oslash;vring
+										</td>
+									</tr>
+									</tbody>
+								</table>
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+									<tr>
+										<td width="600" height="15">&nbsp;</td>
+									</tr>
+									</tbody>
+								</table>
+								<!-- </FOOTER> -->
 							</td>
 						</tr>
-						</tbody>
 					</table>
-					<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
-						<tbody>
-						<tr>
-							<td width="600" height="15">&nbsp;</td>
-						</tr>
-						</tbody>
-					</table>
-						<!-- </FOOTER> -->
 				</td>
-				<td>&nbsp;</td>
 			</tr>
 		</tbody>
 	</table>
