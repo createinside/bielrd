e15537f153b3bb379052749bc64796320806bdd6
Jonas Iversen <ji@peytz.dk>
2016-11-10 12:01:40 +0100

    news template styling - rebildporten and changes to the custom nl_br module

---
+++ sites/all/modules/createinside/modules/nl_br/templates/nl_rp_export_markup.tpl.php
@@ -0,0 +1,294 @@
+<?php global $base_url; ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="format-detection" content="telephone=no">
+	<style type="text/css">
+		html {
+			width:100%;
+		}
+
+		body {
+			width:100% !important;
+			-webkit-text-size-adjust:100%;
+			-ms-text-size-adjust:100%;
+			margin:0;
+			padding:0;
+			background:#f5f5f5;
+		}
+
+		table {
+			border-collapse:collapse;
+			mso-table-lspace:0pt;
+			mso-table-rspace:0pt;
+		}
+
+		table td {
+			border-collapse:collapse;
+		}
+
+		p {
+			margin: 0;
+		}
+
+		img {
+			border:none;
+			display: block;
+			font-size:14px;
+			font-weight:bold;
+			height:auto;
+			line-height:100%;
+			outline:none;
+			text-decoration:none;
+			text-transform:capitalize;
+			-ms-interpolation-mode:bicubic;
+		}
+        .showy {
+          height: 100% !important;
+          width: 100% !important;
+        }
+
+        /* Hide this everywhere, except for those that can’t read this code */
+        .no-showy {
+          display: none;
+            width: 100%;
+        }
+        .showy-footer {
+          width: 100% !important;
+        }
+
+        /* Hide this everywhere, except for those that can’t read this code */
+        .no-showy-footer {
+          display: none;
+            width: 100%;
+        }
+        .showy-face {
+          height: 100% !important;
+          width: 100% !important;
+        }
+
+        /* Hide this everywhere, except for those that can’t read this code */
+        .no-showy-face {
+          display: none;
+        }
+
+		a:link{
+			text-decoration:none;
+			font-weight:normal;
+			color:#282828;
+		}
+
+		a:visited{
+			text-decoration:none;
+			font-weight:normal;
+			color:#282828;
+		}
+
+		a:hover{
+			text-decoration:none;
+			font-weight:normal;
+			color:#282828;
+		}
+
+		a:active{
+			text-decoration:none;
+			font-weight:normal;
+			color:#282828;
+		}
+
+		a[href^=tel],a[href^=sms]{
+			text-decoration:none;
+			color:#282828;
+			cursor:default;
+		}
+
+			a img{
+				border:none;
+			}
+
+		/**
+	 	 * Sektion 3: Tekst med topbillede
+	 	 */
+		.field-name-field-nl-br-sec-3-read-more a {
+			text-decoration: none;
+			color: #ffffff;
+			text-align:center;
+		}
+
+		/**
+	 	 * Sektion 4: Tekst med venstrestillet billede
+	 	 */
+		.field-name-field-nl-br-sec-4-read-more a {
+			text-decoration: none;
+			color: #ffffff;
+			text-align:center;
+		}
+
+		/**
+		 * Sektion 5: Tekst med højrestillet billede
+		 */
+		.field-name-field-nl-br-sec-5-read-more a {
+			text-decoration: none;
+			color: #ffffff;
+			text-align:center;
+		}
+
+		/**
+		 * Sektion 6: Tekst
+		 */
+		.field-name-field-nl-br-sec-6-read-more a {
+			text-decoration: none;
+			color: #ffffff;
+			text-align:center;
+		}
+
+	</style>
+</head>
+<body style="margin:0px; padding:0px; background:#f5f5f5;">
+	<table bgcolor="#f5f5f5" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+		<tbody>
+			<tr>
+				<td align="center" valign="top">
+					<table bgcolor="#f5f5f5" border="0" cellpadding="0" cellspacing="0" width="600">
+						<tr>
+							<td align="center" valign="top">
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="600" height="15">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+								<!-- <HEADER> -->
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="600" height="25" style="font-family: Arial, sans-serif; font-size: 11px; color: #999999; text-align: center;">Vises denne mail ikke korrekt? <a href="%%webversion%%" style="text-decoration: none; color: #999999">Åbn i din browser - Klik her</a></td>
+										</tr>
+									</tbody>
+								</table>
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="600" height="10">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="600" height="15" style="font-family: Arial, sans-serif; font-size: 11px; color: #282828; text-align: center;"><a href="https://www.facebook.com/RebildPorten/" target="_blank" style="text-decoration: none; color: #282828"><!-- Image: SVG -->
+<img class="showy-face" width="0" height="0" src="<?php print $base_url."/".drupal_get_path("module", "nl_br"); ?>/img/facebook.svg">
+<!-- Image: png -->
+<img class="no-showy-face" src="<?php print $base_url."/".drupal_get_path("module", "nl_br"); ?>/img/facebook.png"></a></td>
+										</tr>
+									</tbody>
+								</table>
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="600" height="15">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+								<table bgcolor="#ef5125" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+										<tr>
+											<td width="30" height="25" colspan="4">&nbsp;</td>
+										</tr>
+										<tr>
+											<td width="30" height="15">&nbsp;</td>
+											<td width="270" valign="middle" align="left" style="font-family: Arial, sans-serif; font-size: 20px; text-align: left;">
+												<a style="text-decoration: none;" target="_blank" href="http://www.rebildporten.dk/RebildPorten"><!-- Image: SVG -->
+<img class="showy" width="0" height="0" src="<?php print $base_url."/".drupal_get_path("module", "nl_br"); ?>/img/logo-header.svg">
+<!-- Image: png -->
+<img class="no-showy" src="<?php print $base_url."/".drupal_get_path("module", "nl_br"); ?>/img/logo-header.png"></a>
+											</td>
+											<td width="270" valign="bottom" align="right" style="font-family: Arial, sans-serif; font-size: 16px; line-height: 14px; color: #ffffff; text-align: right;">
+                                                NYHEDSBREV<br /><span style="line-height: 20px;font-size: 11px;"><?php 
+$weekday = array( 
+"Søndag","Mandag","Tirsdag","Onsdag", 
+"Torsdag","Fredag","Lørdag" 
+); 
+$months = array( 
+"Januar","Februar","Marts","April","Maj","Juni", 
+"Juli","August","September","Oktober","November","December" 
+); 
+list($wday,$mday,$month,$year) = preg_split("( )",date("w j n Y")); 
+$month -= 1; 
+$this_day = "$weekday[$wday], $mday. $months[$month] $year"; 
+echo $this_day;
+?> </span>
+											</td>
+											<td width="30" height="15">&nbsp;</td>
+										</tr>
+										<tr>
+											<td width="600" height="25" colspan="4">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+								<!-- </HEADER> -->
+								<!-- <CONTENT> -->
+                                <?php
+									foreach($variables['sections'] as $section) {
+										print $section;
+									}
+								?>
+								<!-- </CONTENT> -->
+								<!-- <FOOTER> -->
+								<table bgcolor="#000000" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+                                        <tr>
+											<td width="250" height="25">&nbsp;</td>
+											<td width="100" height="25">&nbsp;</td>
+											<td width="250" height="25">&nbsp;</td>
+										</tr>
+										<tr>
+											<td width="271">&nbsp;</td>
+											<td align="center" width="59" style="font-family: Arial, sans-serif; font-size: 18px; color: #282828; text-align: center;"><img class="showy-footer" width="0" height="0" src="<?php print $base_url."/".drupal_get_path("module", "nl_br"); ?>/img/logo-footer.svg">
+<!-- Image: png -->
+<img class="no-showy-footer" src="<?php print $base_url."/".drupal_get_path("module", "nl_br"); ?>/img/logo-footer.png"></td>
+											<td width="270" height="15">&nbsp;</td>
+										</tr>
+                                        <tr>
+											<td width="250" height="25">&nbsp;</td>
+											<td width="100" height="25">&nbsp;</td>
+											<td width="250" height="25">&nbsp;</td>
+										</tr>
+									</tbody>
+								</table>
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+									<tr>
+										<td width="600" height="25">&nbsp;</td>
+									</tr>
+									<tr>
+										<td width="600" style="font-family: Arial, sans-serif; font-size: 11px; line-height: 14px; color: #282828; text-align: center;">
+											<p>
+												&Oslash;nsker du at afmelde %%emailaddress%% fra denne liste - <a href="%%unsubscribelink%%" style="text-decoration: underline; color: #282828">klik her</a>.
+
+												<br>
+												<br>Copyright &copy; <?php echo date('Y') ?> RebildPorten
+												<br>Rebildvej 25A, Rebild DK-9520 Skørping
+										</td>
+									</tr>
+									</tbody>
+								</table>
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-size: 0px;">
+									<tbody>
+									<tr>
+										<td width="600" height="15">&nbsp;</td>
+									</tr>
+									</tbody>
+								</table>
+								<!-- </FOOTER> -->
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
