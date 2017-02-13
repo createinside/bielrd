20541ba3303029a096a02d35914fc82019cf0342
Henrik Bak Thomassen <hbt@peytz.dk>
2017-02-13 10:48:28 +0100

    Added GIS overlay feature.

--- sites/all/themes/rebild/template.php
+++ sites/all/themes/rebild/template.php
@@ -11,22 +11,22 @@
  * for more information on this topic.
  */
 
-
 /**
- * Respond.js
+ * Respond.js.
  *
- * Making Respond.js work as intended
+ * Making Respond.js work as intended.
  */
 function rebild_css_alter(&$css) {
-	foreach ($css as $key => $value) {
-      if (preg_match('/^ie::(\S*)/', $key)) {
-          unset($css[$key]);
-      } else {
-          $css[$key]['browsers']['IE'] = TRUE;
-      }
+  foreach ($css as $key => $value) {
+    if (preg_match('/^ie::(\S*)/', $key)) {
+      unset($css[$key]);
+    }
+    else {
+      $css[$key]['browsers']['IE'] = TRUE;
+    }
   }
 
-	// Disabl jQuery UI CSS files
+  // Disabl jQuery UI CSS files.
   $disabled_drupal_css = array(
     // Remove jquery.ui css files.
     'misc/ui/jquery.ui.core.css',
@@ -48,10 +48,10 @@ function rebild_css_alter(&$css) {
  */
 function rebild_delta_blocks_page_title($variables) {
 
-	// Change markup for the business section title
-	if(current_path()=='erhverv' || current_path()=='business') {
-		$variables['page_title'] = '<span>Business</span> Rebild';
-	}
+  // Change markup for the business section title.
+  if (current_path() == 'erhverv' || current_path() == 'business') {
+    $variables['page_title'] = '<span>Business</span> Rebild';
+  }
   if ($variables['page_title'] !== '') {
     $attributes['id'] = 'page-title';
     $attributes['class'][] = 'title';
@@ -64,43 +64,43 @@ function rebild_delta_blocks_page_title($variables) {
 }
 
 /**
- * Preprocess HTML
+ * Preprocess HTML.
  *
- * Implements hook_preprocess_html
+ * Implements hook_preprocess_html.
  */
 function rebild_preprocess_html(&$variables) {
-  
-  // Add jQuery UI
+
+  // Add jQuery UI.
   drupal_add_library('system', 'ui');
   drupal_add_library('system', 'ui.accordion');
 
-	// Facebook Tracking Code (Temporary campaign)
-	$fb_paths = array(
-		'erhverv/mauris-blandit-aliquet-elit',
-		'erhverv/start-virksomhed',
-		'kalender/ivaerksaetteraften-2015',
-		'erhverv/start-virksomhed/raadgivning-hos-ivaerksaetterkonsulent',
-		'erhverv/start-virksomhed/vaeksthusprogrammer'
-	);
-	if(in_array(drupal_get_path_alias(), $fb_paths)) {
+  // Facebook Tracking Code (Temporary campaign)
+  $fb_paths = array(
+    'erhverv/mauris-blandit-aliquet-elit',
+    'erhverv/start-virksomhed',
+    'kalender/ivaerksaetteraften-2015',
+    'erhverv/start-virksomhed/raadgivning-hos-ivaerksaetterkonsulent',
+    'erhverv/start-virksomhed/vaeksthusprogrammer',
+  );
+  if (in_array(drupal_get_path_alias(), $fb_paths)) {
     drupal_add_js(path_to_theme() . '/js/fb_tracker.js',
-    	array(
-		    'group' => JS_THEME,
-		    'preprocess' => TRUE,
-		    'scope' => 'footer',
-		    'weight' => '999',
-	    )
-    );
-	}
+        array(
+          'group' => JS_THEME,
+          'preprocess' => TRUE,
+          'scope' => 'footer',
+          'weight' => '999',
+        )
+     );
+  }
 
-  // Adds Swipebox CSS + JS
+  // Adds Swipebox CSS + JS.
   drupal_add_css('sites/all/libraries/swipebox/source/swipebox.css');
   drupal_add_js('sites/all/libraries/swipebox/source/jquery.swipebox.min.js');
 
-  // Get a list of all the regions for this theme
+  // Get a list of all the regions for this theme.
   foreach (system_region_list($GLOBALS['theme']) as $region_key => $region_name) {
 
-    // Get the content for each region and add it to the $region variable
+    // Get the content for each region and add it to the $region variable.
     if ($blocks = block_get_blocks_by_region($region_key)) {
       $variables['region'][$region_key] = $blocks;
     }
@@ -111,78 +111,79 @@ function rebild_preprocess_html(&$variables) {
 }
 
 /**
- * Implements hook_ultimenu_link
+ * Implements hook_ultimenu_link.
  *
  * Inserts icon in menu link (Mega Menu - Level 1)
  */
 function rebild_ultimenu_link(array $variables) {
 
- 	$element = $variables['element'];
+  $element = $variables['element'];
   $sub_menu = '';
 
   if ($element['#below']) {
     $sub_menu = drupal_render($element['#below']);
   }
   $element['#localized_options']["html"] = TRUE;
-  $output = l($element['#title'].' <span class="icon-arrow-down2"></span>', $element['#href'], $element['#localized_options']);
+  $output = l($element['#title'] . ' <span class="icon-arrow-down2"></span>', $element['#href'], $element['#localized_options']);
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
 
 /**
- * Implements hook_menu_link
+ * Implements hook_menu_link.
  *
  * Inserts icon in menu link (Mega Menu - Level 2+)
  */
 function rebild_menu_link(array $variables) {
 
- 	$element = $variables['element'];
+  $element = $variables['element'];
   $sub_menu = '';
 
   if ($element['#below']) {
     $sub_menu = drupal_render($element['#below']);
   }
-  if(($element['#original_link']['menu_name'] == "main-menu" || $element['#original_link']['menu_name'] == "menu-main-menu-en")
-		&& $element['#original_link']['depth']>1) {
-	  $element['#localized_options']["html"] = TRUE;
-  	$output = l('<span class="icon-double-angle-right"></span> '.$element['#title'], $element['#href'], $element['#localized_options']);
-	}
-	else {
-  	$output = l($element['#title'], $element['#href'], $element['#localized_options']);
-	}
+  if (($element['#original_link']['menu_name'] == "main-menu" || $element['#original_link']['menu_name'] == "menu-main-menu-en")
+        && $element['#original_link']['depth'] > 1) {
+    $element['#localized_options']["html"] = TRUE;
+    $output = l('<span class="icon-double-angle-right"></span> ' . $element['#title'], $element['#href'], $element['#localized_options']);
+  }
+  else {
+    $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  }
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
 
 /**
- * Breadcrumbs
+ * Breadcrumbs.
  *
- * Customizes the breadcrumbs structure
+ * Customizes the breadcrumbs structure.
  */
 function rebild_breadcrumb($variables) {
 
-	$breadcrumb = $variables['breadcrumb'];
+  $breadcrumb = $variables['breadcrumb'];
 
   if (!empty($breadcrumb)) {
-    array_shift($breadcrumb); // Removes the Home item
-    array_unshift($breadcrumb,l(' <span class="icon-home"></span> ','<front>', array("html" => TRUE)));
+    // Removes the Home item.
+    array_shift($breadcrumb);
+    array_unshift($breadcrumb, l(' <span class="icon-home"></span> ', '<front>', array("html" => TRUE)));
     $output = '<div class="breadcrumb">';
     $output .= implode(' <span class="icon-angle-right"></span> ', $breadcrumb);
-    $output .= ' <span class="icon-angle-right"></span> <strong>'.drupal_get_title().'</strong>';
+    $output .= ' <span class="icon-angle-right"></span> <strong>' . drupal_get_title() . '</strong>';
     $output .= '</div>';
     return $output;
   }
 }
 
 /**
- * Preprocess Node
+ * Preprocess Node.
  *
- * Adds a new block region inside the node template
+ * Adds a new block region inside the node template.
  */
 function rebild_preprocess_node(&$variables) {
 
-  // Get a list of all the regions for this theme
+  // Get a list of all the regions for this theme.
   foreach (system_region_list($GLOBALS['theme']) as $region_key => $region_name) {
 
-    // Get the content for each region and add it to the $region variable
+    // Get the content for each region and add it to the $region variable.
     if ($blocks = block_get_blocks_by_region($region_key)) {
       $variables['region'][$region_key] = $blocks;
     }
@@ -193,38 +194,51 @@ function rebild_preprocess_node(&$variables) {
 }
 
 /**
- * Preprocess Field
+ * Preprocess Field.
  *
- * Implements hook_preprocess_field
+ * Implements hook_preprocess_field.
  */
 function rebild_preprocess_field(&$variables, $hook) {
-  
-	$element = $variables['element'];
 
-	// GIS KORT
+  $element = $variables['element'];
+
+  // GIS KORT.
   if ($element['#field_name'] == 'field_main_gis_map') {
-		// Hide GIS map on mobile devices
-  	$browser = browscap_get_browser();
-  	if($browser["ismobiledevice"]=="false") {
-  		$variables["items"][0]["#markup"] = "<iframe width='100%' height='400px' src='".$variables["items"][0]["#markup"]."'> </iframe>";
-  	}
-  	else {
-	  	$variables["items"][0]["#markup"] = "<a href='".$variables["items"][0]["#markup"]."'>Se kort her</a>";
-  	}
+
+    $entity_type = $element['#entity_type'];
+    $overlay = $element['#object']->field_main_gis_overlay['und'][0]['value'];
+    $url = $variables["items"][0]["#markup"];
+    $browser = browscap_get_browser();
+
+    // Hide GIS map on mobile devices.
+    if ($browser["ismobiledevice"] == "false") {
+      $variables["items"][0]["#markup"] = "<iframe id='gis-map-frame' width='100%' height='400px' src='" . $url . "' > </iframe>";
+
+      // Check if overlay should be enabled.
+      if ($overlay) {
+        $variables["items"][0]["#markup"] .= "<a href='?width=1024&height=768&inline=true#gis-map-frame' class='colorbox-inline'>Se større kort</a>";
+      }
+    }
+    else {
+      $variables["items"][0]["#markup"] = "<a href='" . $url . "'>Se kort her</a>";
+    }
   }
 }
 
+/**
+ *
+ */
 function rebild_picture($variables) {
 
-	// Set alt and title attributes
-	if(isset($variables['field_file_image_alt_text']['und'][0]['value'])) {
-		$variables["alt"] = $variables['field_file_image_alt_text']['und'][0]['value'];
-		// Title attribute equals value of alt attribute
-		$variables["title"] = $variables['field_file_image_alt_text']['und'][0]['value'];
-	}
+  // Set alt and title attributes.
+  if (isset($variables['field_file_image_alt_text']['und'][0]['value'])) {
+    $variables["alt"] = $variables['field_file_image_alt_text']['und'][0]['value'];
+    // Title attribute equals value of alt attribute.
+    $variables["title"] = $variables['field_file_image_alt_text']['und'][0]['value'];
+  }
 
-	// Make sure that width and height are proper values
-  // If they exists we'll output them
+  // Make sure that width and height are proper values
+  // If they exists we'll output them.
   // @see http://www.w3.org/community/respimg/2012/06/18/florians-compromise/
   if (isset($variables['width']) && empty($variables['width'])) {
     unset($variables['width']);
@@ -318,37 +332,43 @@ function rebild_preprocess_search_results(&$variables) {
   $variables['theme_hook_suggestions'][] = 'search_results__' . $variables['module'];
 }
 
+/**
+ *
+ */
 function rebild_page_alter(&$page) {
 
-	$node = menu_get_object();
-	// Breadcrumb: Agenda nodes
-	if(!empty($node)) {
-		if($node->type=="agenda") {
+  $node = menu_get_object();
+  // Breadcrumb: Agenda nodes.
+  if (!empty($node)) {
+    if ($node->type == "agenda") {
 
-			$committee = field_get_items('node', $node, 'field_agenda_committee');
+      $committee = field_get_items('node', $node, 'field_agenda_committee');
 
-			$breadcrumb = drupal_get_breadcrumb();
-			$breadcrumb[] = l('Politik', 'politik');
-			$breadcrumb[] = l('Dagsordener og referater', 'politik/dagsordener-og-referater');
-			$breadcrumb[] = l($committee[0]['taxonomy_term']->name, 'politik/dagsordener-og-referater/'.$committee[0]["tid"]);
-			drupal_set_breadcrumb($breadcrumb);
-		}
-	// Breadcrumb: Agenda views
-		else if(arg(1) == 'dagsordener-og-referater' && is_numeric(arg(2))) {
-			$breadcrumb = drupal_get_breadcrumb();
-			array_pop($breadcrumb);
-			drupal_set_breadcrumb($breadcrumb);
-		}
-	}
+      $breadcrumb = drupal_get_breadcrumb();
+      $breadcrumb[] = l('Politik', 'politik');
+      $breadcrumb[] = l('Dagsordener og referater', 'politik/dagsordener-og-referater');
+      $breadcrumb[] = l($committee[0]['taxonomy_term']->name, 'politik/dagsordener-og-referater/' . $committee[0]["tid"]);
+      drupal_set_breadcrumb($breadcrumb);
+    }
+    // Breadcrumb: Agenda views.
+    elseif (arg(1) == 'dagsordener-og-referater' && is_numeric(arg(2))) {
+      $breadcrumb = drupal_get_breadcrumb();
+      array_pop($breadcrumb);
+      drupal_set_breadcrumb($breadcrumb);
+    }
+  }
 
-}
-
-function rebild_date_all_day_label() {
-	return '';
 }
 
 /**
- * CALENDAR TITLES
+ *
+ */
+function rebild_date_all_day_label() {
+  return '';
+}
+
+/**
+ * CALENDAR TITLES.
  */
 function rebild_date_nav_title($params) {
   $granularity = $params['granularity'];
@@ -361,24 +381,27 @@ function rebild_date_nav_title($params) {
       $title = $date_info->year;
       $date_arg = $date_info->year;
       break;
+
     case 'month':
       $format = !empty($format) ? $format : (empty($date_info->mini) ? 'F Y' : 'F Y');
       $title = date_format_date($date_info->min_date, 'custom', $format);
       $date_arg = $date_info->year . '-' . date_pad($date_info->month);
       break;
+
     case 'day':
       $format = !empty($format) ? $format : (empty($date_info->mini) ? 'l, d. F' : 'l, F j');
       $title = date_format_date($date_info->min_date, 'custom', $format);
       $date_arg = $date_info->year . '-' . date_pad($date_info->month) . '-' . date_pad($date_info->day);
       break;
+
     case 'week':
       $format = !empty($format) ? $format : (empty($date_info->mini) ? 'W, Y' : 'F j');
-			if($date_info->week == 1 || $date_info->week == 53) {
-				$max_date = $date_info->max_date->sub( new DateInterval('PT1S') );
-	      $title = t('Uge @date', array('@date' => date_format_date($max_date, 'custom', $format)));
-			}
-			else {
-	      $title = t('Uge @date', array('@date' => date_format_date($date_info->min_date, 'custom', $format)));
+      if ($date_info->week == 1 || $date_info->week == 53) {
+        $max_date = $date_info->max_date->sub(new DateInterval('PT1S'));
+        $title = t('Uge @date', array('@date' => date_format_date($max_date, 'custom', $format)));
+      }
+      else {
+        $title = t('Uge @date', array('@date' => date_format_date($date_info->min_date, 'custom', $format)));
       }
       $date_arg = $date_info->year . '-W' . date_pad($date_info->week);
       break;
@@ -393,18 +416,19 @@ function rebild_date_nav_title($params) {
     return $title;
   }
 }
+
 /**
- * hook_menu_local_task_alter
+ * Hook_menu_local_task_alter.
  *
  * Convert Clone link from action link to tab.
  */
 function rebild_menu_local_tasks_alter(&$data, $router_item, $root_path) {
-	if (isset($data['actions']['output'][0])) {
-		if($data['actions']['output'][0]['#link']['page_callback'] == 'clone_node_check') {
-			$data['actions']['output'][0]['#theme'] = 'menu_local_task';
-			$data['actions']['output'][0]['#link']['title'] = 'Opret kopi';
-			$data['tabs'][0]['output'][] = $data['actions']['output'][0];
-			unset($data['actions']['output'][0]);
-		}
-	}
+  if (isset($data['actions']['output'][0])) {
+    if ($data['actions']['output'][0]['#link']['page_callback'] == 'clone_node_check') {
+      $data['actions']['output'][0]['#theme'] = 'menu_local_task';
+      $data['actions']['output'][0]['#link']['title'] = 'Opret kopi';
+      $data['tabs'][0]['output'][] = $data['actions']['output'][0];
+      unset($data['actions']['output'][0]);
+    }
+  }
 }
